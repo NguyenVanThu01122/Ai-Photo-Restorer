c30b8eed304c01bf1fef352a8a8b010acc934424
nguyen van thu <vanthu01122@gmail.com>
2023-12-23 20:25:21 +0700

    fix bug

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,7 +4,7 @@ import GoogleProvider from "next-auth/providers/google";
 import prisma from "../../../lib/prismadb";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
+  // adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
