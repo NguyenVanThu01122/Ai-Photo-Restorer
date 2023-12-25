c6f7622caa3a12cc9e45d434c9379c6433ef6229
nguyen van thu <vanthu01122@gmail.com>
2023-12-25 14:03:27 +0700

    fix nextauth_secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,7 +9,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET || "",
 };
 
 export default NextAuth({
